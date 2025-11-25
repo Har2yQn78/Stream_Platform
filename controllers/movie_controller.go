bcb5545777ba07023cd12bac8b59c53fc49b86fb
Har2yQn78 <hamidreza.amiri800@gmail.com>
2025-11-25 16:29:48 +0330

    add getMovieID API

--- controllers/movie_controller.go
+++ controllers/movie_controller.go
@@ -36,3 +36,27 @@ func GetMovies() gin.HandlerFunc {
 		c.JSON(http.StatusOK, movies)
 	}
 }
+
+func GetMovieById() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		movieID := c.Param("imdb_id")
+
+		if movieID == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "movie id is empty"})
+			return
+		}
+		var movie models.Movie
+
+		err := movieCollection.FindOne(ctx, bson.M{"_id": movieID}).Decode(&movie)
+
+		if err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, movie)
+	}
+}
